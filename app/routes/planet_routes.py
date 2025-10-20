9c3ea8753658b6fa7fb44bbf69535a130b55447b
Madi Rei <madison.nicole.rei@gmail.com>
2025-10-20 11:25:30 -0700

    adds functionality to read one book endpoint

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, abort
 from app.models.planets import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
@@ -17,9 +17,33 @@ def get_all_planets():
 
     return results_list
 
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "size": planet.size
+    }
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        response = {"message": f"Planet {planet_id} is invalid."}
+        abort(make_response(response, 400))
+
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+
+    response = {"message": f"Planet ID {planet_id} not found."}
+    abort(make_response(response, 404))
 
 
 
 
-
+ 
         
