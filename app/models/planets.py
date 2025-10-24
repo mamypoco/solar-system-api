a9897ede8f402c9869eb486170c19bda7e364d9b
Madi Rei <madison.nicole.rei@gmail.com>
2025-10-24 13:40:13 -0700

    Adds Planets Model

--- app/models/planets.py
+++ app/models/planets.py
@@ -1,14 +1,8 @@
-class Planets:
-    def __init__(self, id, name, description, size):
-        self.id = id 
-        self.name = name 
-        self.description = description
-        self.size = size
+from ..db import db
+from sqlalchemy.orm import Mapped, mapped_column
 
-
-planets = [
-    Planets(1, "Mars", "dusty", "medium"),
-    Planets(2, "Jupiter", "1000 times the size of earth", "large"),
-    Planets(3, "Mercury", "closest to Sun", "large"),
-    Planets(4, "Uranus", "cold", "small")
-] 
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    size: Mapped[str]
--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,49 +1,45 @@
-from flask import Blueprint, make_response, abort
-from app.models.planets import planets
+from flask import Blueprint, make_response, abort, request
+from app.models.planets import Planet
+from ..db import db
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
+@planets_bp.post("")
+def create_planet():
+    request_body = request.get_json()
+    name = request_body["name"]
+    description = request_body["description"]
+    size = request_body["size"]
+
+    new_planet = Planet(
+        name=name,
+        description=description,
+        size=size
+    )
+    db.session.add(new_planet)
+    db.session.commit()
+
+    planet_response = dict(
+        id=new_planet.id,
+        name=new_planet.name,
+        description=new_planet.description,
+        size=new_planet.size
+    )
+
+    return planet_response, 201
+
 @planets_bp.get("")
 def get_all_planets():
-    results_list = []
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+    result_list = []
 
     for planet in planets:
-        results_list.append(dict(
-            id = planet.id,
-            name = planet.name,
-            description = planet.description,
-            size = planet.size        
+        result_list.append(dict(
+            id=planet.id,
+            name=planet.name,
+            description=planet.description,
+            size=planet.size
         ))
 
-    return results_list
-
-@planets_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
-
-    return {
-        "id": planet.id,
-        "name": planet.name,
-        "description": planet.description,
-        "size": planet.size
-    }
-
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except ValueError:
-        response = {"message": f"Planet {planet_id} is invalid."}
-        abort(make_response(response, 400))
-
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
-
-    response = {"message": f"Planet ID {planet_id} not found."}
-    abort(make_response(response, 404))
-
-
-
-
- 
-        
+    return result_list
