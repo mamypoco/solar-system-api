cbf325e28ff351804d6b21ca60366664c6c81fd7
mami <mamypoco@gmail.com>
2025-10-31 14:34:03 -0700

    updated to fix typo

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLACHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     if config:
         app.config.update(config)
